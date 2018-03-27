3f797fce1e024e58dd02a62ab7e44be7fff2d52f
Nanchen Yang <ncyang@thoughtworks.com>
2018-03-27 23:00:05 +0800

    defined converter in data layer

--- app/src/main/java/tw/helloandroid/domain/model/DomainClasses.kt
+++ app/src/main/java/tw/helloandroid/domain/model/DomainClasses.kt
@@ -4,6 +4,8 @@ data class ForecastList(val id: Long, val city: String, val country: String,
                         val dailyForecast: List<Forecast>) {
     val size: Int
         get() = dailyForecast.size
+
+    operator fun get(position: Int) = dailyForecast[position]
 }
 
 data class Forecast(val id: Long, val date: Long, val description: String,
--- app/src/main/java/tw/helloandroid/ui/adapters/ForecastListAdapter.kt
+++ app/src/main/java/tw/helloandroid/ui/adapters/ForecastListAdapter.kt
@@ -1,28 +1,40 @@
 package tw.helloandroid.ui.adapters
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_forecast.view.*
+import tw.helloandroid.R
 import tw.helloandroid.domain.model.Forecast
 import tw.helloandroid.domain.model.ForecastList
+import tw.helloandroid.extensions.toDateString
 
-class ForecastListAdapter(weekForecast: ForecastList) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+class ForecastListAdapter(val weekForecast: ForecastList) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view = LayoutInflater.from(parent?.context)
+                .inflate(R.layout.item_forecast, parent, false)
+        return ViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getItemCount(): Int = weekForecast.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.bindForecast(weekForecast[position])
     }
 
-    class ViewHolder(view: View, private val click: (Forecast) -> Unit): RecyclerView.ViewHolder(view) {
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
         fun bindForecast(forecast: Forecast) {
-
+            with(forecast) {
+                itemView.let {
+                    Picasso.get().load(iconUrl).into(it.icon)
+                    it.date.text = date.toDateString()
+                    it.description.text = description
+                    it.maxTemperature.text = "${high}º"
+                    it.minTemperature.text = "${low}º"
+                }
+            }
         }
     }
-
 }
