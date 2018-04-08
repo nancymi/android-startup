bf80bc490e57a0b26ef2df90bb2cdd7a310b99fd
Nanchen Yang <ncyang@thoughtworks.com>
2018-04-08 17:28:06 +0800

    defined ForecastDataSource interface.

--- app/src/main/java/tw/helloandroid/domain/datasource/ForecastDataSource.kt
+++ app/src/main/java/tw/helloandroid/domain/datasource/ForecastDataSource.kt
@@ -1,2 +1,7 @@
 package tw.helloandroid.domain.datasource
 
+import tw.helloandroid.domain.model.ForecastList
+
+interface ForecastDataSource {
+    fun requestForecastByZipCode(zipCode: Long, date: Long): ForecastList?
+}
