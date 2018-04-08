19fe26b0ba81ebc791e21e75fe1565c4707e0570
Nanchen Yang <ncyang@thoughtworks.com>
2018-04-08 17:29:27 +0800

    added forecast server source to ForecastProvider.

--- app/src/main/java/tw/helloandroid/domain/datasource/ForecastProvider.kt
+++ app/src/main/java/tw/helloandroid/domain/datasource/ForecastProvider.kt
@@ -1,4 +1,9 @@
 package tw.helloandroid.domain.datasource
 
-class ForecastProvider {
+import tw.helloandroid.data.server.ForecastServer
+
+class ForecastProvider(private val sources: List<ForecastDataSource> = SOURCE) {
+    companion object {
+        val SOURCE by lazy { listOf(ForecastServer()) }
+    }
 }
